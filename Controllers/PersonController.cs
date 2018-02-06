a01e303f5563f0ea417adfd795c2f7b9bd1cfb7d
AltTether <alt_tether@yahoo.co.jp>
2018-02-06 21:36:17 +0900

    FaceAPIを呼び出すクラスと関数のプログラムの作成

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using registerPerson.Models;
+using System.Web;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Net.Http;
+
+using System.IO;
+using System.Net;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Web.Script.Serialization;
+using System.Configuration;
+using System.Diagnostics;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace registerPerson.Controllers
+{
+    [Route("api/[controller]")]
+    public class PersonController : Controller
+    {
+        [HttpPost]
+        public string Post([FromBody]Person person)
+        {
+            string appKey = ConfigurationManager.AppSettings["appKey"];
+
+            var faceApi = new FaceApi()
+            {
+                SubscriptionKey = appKey,
+                GroupName = "group1",
+            };
+            var task = Task.Run(() =>
+            {
+                return faceApi.createPerson(person.Name);
+            });
+            return task.Result;
+        }
+    }
+}
---
+++ Models/FaceApi.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Net.Http;
+using System.Collections;
+using Newtonsoft.Json;
+using System.Web;
+using System.Net.Http.Headers;
+
+namespace registerPerson.Models
+{
+    public class FaceApi
+    {
+        public string SubscriptionKey { get; set; }
+        public string GroupName { get; set; }
+
+        async public Task<string> createPerson(String name)
+        {
+            var personInfo = new Person()
+            {
+                Name = name,
+            };
+
+            var json = JsonConvert.SerializeObject(personInfo);
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", SubscriptionKey);
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "https://australiaeast.api.cognitive.microsoft.com/face/v1.0/persongroups/group1/persons");
+                request.Content = new System.Net.Http.StringContent(json, Encoding.UTF8, "application/json");
+                var res = await client.SendAsync(request);
+                return res.Content.ReadAsStringAsync().Result;
+            }
+        }
+    }
+}
