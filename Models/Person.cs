7f76c98d510ee1172c1c716dc00a5e1f3e3c8cba
AltTether <200351p@ugs.kochi-tech.ac.jp>
2018-02-12 21:39:53 +0900

    名前と顔写真URLは一度に取得するためPersonクラスにFaceImgUrlを追加

--- Models/Person.cs
+++ Models/Person.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace registerPerson.Models
 {
     public class Person
     {
         public string Name { get; set; }
+        public string FaceImgUrl { get; set; }
     }
 }
