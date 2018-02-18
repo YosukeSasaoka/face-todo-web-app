53b47a1d09099e326af49a178e631f1c0ec55011
KATAOKA TAICHI <komaki.1027.math@gmail.com>
2018-02-18 16:37:16 +0900

    Startup.cs(DB接続)追加

--- faceTodoApplication/Startup.cs
+++ faceTodoApplication/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using faceTodoApplication.Models;
 
 namespace faceTodoApplication
 {
@@ -22,6 +24,8 @@ namespace faceTodoApplication
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddDbContext<TodoesContext>(options =>
+            options.UseSqlite("Data Source=faceTodoApplication.db"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,7 +46,7 @@ namespace faceTodoApplication
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Todo}/{action=Create}/{id?}");
             });
         }
     }
