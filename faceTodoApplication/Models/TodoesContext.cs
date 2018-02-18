1dca83e025c5121281bb3acd1726245d6199596f
KATAOKA TAICHI <komaki.1027.math@gmail.com>
2018-02-18 15:59:42 +0900

    Todoクラス(名前、URL)・Dbコンテキスト作成

---
+++ faceTodoApplication/Models/TodoesContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace faceTodoApplication.Models
+{
+    public class TodoesContext : DbContext
+    {
+        public TodoesContext(DbContextOptions<TodoesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<faceTodoApplication.Models.Todo> Todo { get; set; }
+    }
+}
